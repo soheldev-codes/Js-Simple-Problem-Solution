31b27ebe250aa9a2d2e46f5e0539c8ae0fdb3a8b
soheldev-codes <soheldev.codes@gmail.com>
2024-12-09 10:57:17 +0600

    Simple Problem Solution If else condition

---
+++ JS-Conditional-Problem-And-Solution/tasks-1.js
@@ -0,0 +1,20 @@
+/***
+ --Question-1
+
+Free Drinks
+
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+// Solution
+
+const myCash = 500;
+
+if (myCash > 500) {
+    console.log("I Eat Burger And  I Recived A Free Coke.")
+}
+else {
+    console.log("Coke Is Better.Because coke is only 30 taka")
+}
